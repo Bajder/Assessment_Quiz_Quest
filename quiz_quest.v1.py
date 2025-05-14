245697a54f3b24a750bbe233fa535a435435ccb8
Bajder <brooke.jackson@papamoacollege.school.nz>
2025-05-15 10:54:56 +1200

    15/05/2025
    Block One

--- quiz_quest.v1.py
+++ quiz_quest.v1.py
@@ -1,3 +1,4 @@
+import random
 # Date: 13/05/2025
 # Program Name: Quiz Quest
 # Author: Brooke Jackson
@@ -43,27 +44,76 @@ def string_checker(question, valid_ans):
         print()
 
 # Initialise Variables
-question_types = ["triangle", "line", "square"]
+question_types = ["triangle", "line", "square", "xxx"]
 questions_answered = 0
+correct_answers = 0
+incorrect_answers = 0
+question_number = 1
+game_history = []
 # Check how many questions the user would like
 print("Welcome to Quiz Quest - Angles Edition!")
 number_of_questions = int_check("How many rounds would you like? Press <enter> to start infinite mode ")
-# Checks the number of rounds prints correctly - for testing purposes
-print(number_of_questions)
 if number_of_questions == "infinite":
     print("infinite mode")
     mode = "infinite"
     number_of_questions = 2
+else:
+    mode = "regular"
 while questions_answered < number_of_questions:
+    print(f"Question {question_number}")
     # Ask for type of question - Triangle, straight line, square
     type_of_question = string_checker("Choose a question type! (Triangle (T), Straight Line (L), or Square (S)): ", question_types)
     print(type_of_question)
     questions_answered += 1
     if mode == "infinite":
         number_of_questions += 1
+    # Stop the question process if the user enters the exit code
+    if type_of_question == "xxx":
+        break
     if type_of_question == "triangle":
-        # makes the question
-        print("ya da ya da")
+        # Defines the angles in the question so they do not get changed if the value error is triggered and the code runs again
+        angle_one = random.randint(1, 90)
+        angle_two = random.randint(1, 90)
+        while True:
+            try:
+                user_answer = int(input(f'If a triangles internal angles add up to 180 degrees, and the first to degrees are {angle_one} and {angle_two}, what is the value of the third angle? '))
+                answer_triangle = 180 - angle_one - angle_two
+                if user_answer == answer_triangle:
+                    print("Congratulations! You answered correctly")
+                    correct_answers += 1
+
+                else:
+                    # Tells the user they were wrong and displays the correct answer so that they may be able to see what went wrong in their process and so that they may better understand how to answer correctly
+                    print(f"Unfortunately, that is not the right answer. The right answer was {answer_triangle}")
+                    incorrect_answers += 1
+            except ValueError:
+                print("Please enter a valid integer as your answer")
+            question_number += 1
+    if type_of_question == "line":
+        angle_one = random.randint(1, 90)
+        user_answer = int(input(f'If the angles around straight lines add up to 180 degrees, and you know one angle is {angle_one}, what is the value of the adjacent angle?'))
+        answer_line = 180 - angle_one
+        if user_answer == answer_line:
+            print("Congratulations! You answered correctly")
+            correct_answers += 1
+        else:
+            print(f"Unfortunately, that is not the right answer. The right answer was {answer_line}")
+            incorrect_answers += 1
+        question_number += 1
+    if type_of_question == "square":
+        angle_one = random.randint(1, 90)
+        angle_two = random.randint(1, 90)
+        angle_three = random.randint(1, 90)
+        user_answer = int(input(f'You have a quadrilateral sitting in front of you. You know three of the angles: {angle_one}, {angle_two}, {angle_three}, and also that all four angles should add up to 360 degrees total. What is the value of the missing angle? '))
+        answer_square = 360 - angle_one - angle_two - angle_three
+        if user_answer == answer_square:
+            print("Congratulations! You answered correctly")
+            correct_answers += 1
+        else:
+            print(f"Unfortunately, that is not the right answer. The right answer was {answer_square}")
+            incorrect_answers += 1
+    # Collect game history?
+
 # Triangle - random int, random int 180 - both these = answer
 # Straight line 180 - random int
 # Square - 360 - 3 random ints

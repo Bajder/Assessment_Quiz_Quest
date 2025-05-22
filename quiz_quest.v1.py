489c890a08a935f621f31a2fea66cff2a1a59a35
Bajder <brooke.jackson@papamoacollege.school.nz>
2025-05-22 12:09:17 +1200

    22/05/2025 - Block Two

--- quiz_quest.v1.py
+++ quiz_quest.v1.py
@@ -1,5 +1,8 @@
 import random
-# Date: 13/05/2025
+
+
+# Date Started: 13/05/2025
+# Date Completed: 27/05/2025
 # Program Name: Quiz Quest
 # Author: Brooke Jackson
 # Program Use: Mathematics-Based Quiz for Students
@@ -26,6 +29,7 @@ def int_check(question):
         except ValueError:
             print(error)
 
+
 def string_checker(question, valid_ans=("yes", "no")):
     error = f"Please enter a valid option from the following list: {valid_ans}"
 
@@ -44,6 +48,7 @@ def string_checker(question, valid_ans=("yes", "no")):
         print(error)
         print()
 
+
 def the_instructions():
     """ Prints the instructions for the user """
     print("""
@@ -52,17 +57,19 @@ def the_instructions():
 To begin, choose the number of questions you would like to answer
 
 Then you will be asked to select a question type
-This is out of Triangle, Point, or Quadrilateral 
-And try to answer. If you answer incorrectly, you will be told the correct answer before you move on
+This is out of: Triangle, Point, or Quadrilateral 
+And will then have to try to answer. If you answer incorrectly, you will be told the correct answer before you move on
 Remember -
 A triangles internal angles always add up to 180 degrees
 Angles around a point will also always add up to 180 degrees
 And a Quadrilaterals internal angles will always add to 360 degrees.
+You may use a calculator, or, for an extra challenge - try without!
 You can enter the exit code of xxx at any time when selecting a question type if you would like to end the quiz
 And to only type your answer without a unit!
 
 Good luck, and if you get stuck, don't be afraid to look at the question from a different angle 😉
     """)
+
 
 def history():
     """ Appends values to the game history so that the code may be more concise """
@@ -71,6 +78,7 @@ def history():
     # Defines the variable to only be used if the user was incorrect - left blank if they were correct
     history_item = f"Question {question_number} - {question}, {answer_input}. {answer_correct_or_incorrect}. {actual_answer}"
     quiz_history.append(history_item)
+
 
 # Initialise Variables
 question_types = ["triangle", "point", "quadrilateral", "xxx"]
@@ -90,7 +98,8 @@ wants_instructions = string_checker(f"{user_name}, would you like to see the ins
 if wants_instructions == "yes":
     the_instructions()
 # Check how many questions the user would like
-number_of_questions = int_check(f"How many questions would you like, {user_name}? Press <enter> to start infinite mode ")
+number_of_questions = int_check(
+    f"How many questions would you like, {user_name}? Press <enter> to start infinite mode ")
 if number_of_questions == "infinite":
     print(" ∞ infinite mode initiated! ∞ ")
     mode = "infinite"
@@ -100,19 +109,21 @@ else:
 while questions_answered < number_of_questions:
     print(f"Question {question_number}")
     # Ask for type of question - Triangle, point, quadrilateral
-    type_of_question = string_checker("Choose a question type! (Triangle (T), Point (P), or Quadrilateral (Q)): ", question_types)
+    type_of_question = string_checker("Choose a question type! (Triangle (T), Point (P), or Quadrilateral (Q)): ",
+                                      question_types)
     if mode == "infinite":
         number_of_questions += 1
     # Stop the question process if the user enters the exit code
     if type_of_question == "xxx":
         break
-    if type_of_question == "triangle":
+    elif type_of_question == "triangle":
         # Defines the angles in the question so they do not get changed if the value error and loop are triggered
         angle_one = random.randint(1, 90)
         angle_two = random.randint(1, 90)
         while True:
             try:
-                user_answer = int(input(f'If a triangles internal angles add up to 180 degrees, and the first to degrees are {angle_one} and {angle_two}, what is the value of the third angle? '))
+                user_answer = int(input(
+                    f'If a triangles internal angles add up to 180 degrees, and the first to degrees are {angle_one} and {angle_two}, what is the value of the third angle? '))
                 # Triangle - 180 - random int - random int = answer
                 answer_triangle = 180 - angle_one - angle_two
                 if user_answer == answer_triangle:
@@ -140,7 +151,8 @@ while questions_answered < number_of_questions:
         angle_one = random.randint(1, 175)
         while True:
             try:
-                user_answer = int(input(f'If the angles around a point add up to 180 degrees, and you know one angle is {angle_one}, what is the value of the adjacent angle? '))
+                user_answer = int(input(
+                    f'If the angles around a point add up to 180 degrees, and you know one angle is {angle_one}, what is the value of the adjacent angle? '))
                 # Point 180 - random int = answer
                 answer_point = 180 - angle_one
                 if user_answer == answer_point:
@@ -169,8 +181,9 @@ while questions_answered < number_of_questions:
         angle_three = random.randint(1, 90)
         while True:
             try:
-                user_answer = int(input(f'You have a quadrilateral sitting in front of you. You know three of the angles: {angle_one}, {angle_two}, {angle_three}, \t'
-                                        f'and also that all four angles should add up to 360 degrees total. What is the value of the missing angle? '))
+                user_answer = int(input(
+                    f'You have a quadrilateral sitting in front of you. You know three of the angles: {angle_one}, {angle_two}, {angle_three}, \t'
+                    f'and also that all four angles should add up to 360 degrees total. What is the value of the missing angle? '))
                 # Square - 360 - 3 random ints = answer
                 answer_quad = 360 - angle_one - angle_two - angle_three
                 if user_answer == answer_quad:
@@ -188,7 +201,6 @@ while questions_answered < number_of_questions:
                 print("If you wish to skip this question, enter 0 to move on. This way you may see the correct answer")
         # Game History
         question_p1 = f'The Question was: You have a quadrilateral sitting in front of you. You know three of the angles: {angle_one}, {angle_two}, {angle_three},'
-        question_p2 = f'and also that all four angles should add up to 360 degrees total. What is the value of the missing angle?'
         if answer == "incorrect 😔. Keep Working On It!":
             actual_answer = f"The Correct answer was: {answer_quad}"
         else:
@@ -209,18 +221,17 @@ if questions_answered > 0:
     print(f"👍 Correct Answers: {percent_correct:.2f} \t"
           f"😔 Incorrect Answers: {percent_incorrect:.2f}")
     if percent_incorrect > 50:
-      print(f"You might need a new angle to go about these questions with, {user_name}!")
+        print(f"You might need a new angle to go about these questions with, {user_name}!")
     else:
         print(f"Excellent Job, {user_name}! When it comes to angles, you seem to be acute-ly aware.")
     # Game History
     see_history = string_checker("Do you want to see your game history? ")
     if see_history == "yes":
-      print("Game History: ")
-      for item in quiz_history:
-        print(item)
+        print("Game History: ")
+        for item in quiz_history:
+            print(item)
     print()
     print("thanks for playing.")
 # otherwise, roast
 else:
     print(f"Sorry, {user_name}, it seems you've been obtuse, and ended before answering any questions!")
-
